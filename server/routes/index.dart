624c6c4b5daba961ca5fb7b05244dceb161e5f3d
rafaelbarbsoatec <rafaelbarbosatec@gmail.com>
2023-12-22 13:05:00 -0300

    fix route / and /playes

--- server/routes/index.dart
+++ server/routes/index.dart
@@ -1,11 +1,29 @@
 import 'package:dart_frog/dart_frog.dart';
 
 import '../src/core/game.dart';
+import '../src/core/game_map.dart';
+import '../src/game/player.dart';
 
 Future<Response> onRequest(RequestContext context) async {
   // ignore: strict_raw_type
   final game = context.read<Game>();
+
+  var countMap = 0;
+  var countPlayers = 0;
+
+  for (final comp in game.components) {
+    if (comp is GameMap) {
+      final players = comp.components
+          .whereType<Player>()
+          .map((e) => e.state)
+          .map((e) => e.toMap());
+      countPlayers += players.length;
+      countMap++;
+    }
+  }
+
   return Response(
-    body: 'Welcome Game Server\n ${game.components.length} players online.',
+    body:
+        'Welcome Game Server\n $countMap maps \n $countPlayers players online.',
   );
 }
--- server/routes/players.dart
+++ server/routes/players.dart
@@ -3,11 +3,25 @@ import 'dart:convert';
 import 'package:dart_frog/dart_frog.dart';
 
 import '../src/core/game.dart';
+import '../src/core/game_map.dart';
 import '../src/game/player.dart';
 
 Response onRequest(RequestContext context) {
   // ignore: strict_raw_type
   final game = context.read<Game>();
-  final players = game.components.whereType<Player>().map((e) => e.state);
-  return Response(body: jsonEncode(players.map((e) => e.toMap()).toList()));
+
+  final resp = <String, dynamic>{};
+
+  for (final comp in game.components) {
+    if (comp is GameMap) {
+      final players = comp.components
+          .whereType<Player>()
+          .map((e) => e.state)
+          .map((e) => e.toMap())
+          .toList();
+      resp[comp.name] = players;
+    }
+  }
+
+  return Response(body: jsonEncode(resp));
 }
