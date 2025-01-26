e74552bcf09cf4f8d873f06d9d6cb33c616d4e8e
Pessoal Dev <pessoaldev@RafaelBarbosas-MacBook-Pro.local>
2025-01-26 01:50:43 -0300

    chore: adjustmens

--- server/src/infrastructure/websocket/polo_websocket.dart
+++ server/src/infrastructure/websocket/polo_websocket.dart
@@ -17,8 +17,7 @@ class PoloWebsocket extends WebsocketProvider<PoloClient> {
   bool initialized = false;
 
   static final PoloWebsocket _singleton = PoloWebsocket._internal(
-    '0.0.0.0',
-    // '127.0.0.1',
+    '127.0.0.1',
     3000,
   );
 
