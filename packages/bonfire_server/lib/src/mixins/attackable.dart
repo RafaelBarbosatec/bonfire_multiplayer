fccbc968a02a3166880ea7985b23ac819da60480
rafaelalmeidacloudwalk <rafael.almeida@cloudwalk.io>
2026-04-10 00:27:30 -0300

    refactor: Change life and damage types from double to int in Attackable mixin

--- packages/bonfire_server/lib/src/mixins/attackable.dart
+++ packages/bonfire_server/lib/src/mixins/attackable.dart
@@ -1,31 +1,38 @@
 // ignore_for_file: public_member_api_docs
 
 import 'package:bonfire_server/src/components/positioned_game_component.dart';
+import 'package:bonfire_server/src/mixins/use_state.dart';
 
 mixin Attackable on PositionedGameComponent {
-  double _life = 100;
-  double _maxLife = 100;
+  int _life = 100;
+  int _maxLife = 100;
 
-  double get life => _life;
-  double get maxLife => _maxLife;
+  int get life => _life;
+  int get maxLife => _maxLife;
   bool get isDead => _life <= 0;
 
-  set life(double value) {
-    _life = value.clamp(0, _maxLife);
+  set life(int value) {
+    final newLife = value.clamp(0, _maxLife);
+    if (_life != newLife) {
+      _life = newLife;
+      _updateStateLife();
+    }
   }
 
-  void initialLife(double life) {
+  void initialLife(int life) {
     _maxLife = life;
     _life = life;
+    _updateStateLife();
   }
 
-  void receiveDamage(double damage) {
+  void receiveDamage(int damage) {
     if (isDead) return;
 
     final previousLife = _life;
     _life = (_life - damage).clamp(0, _maxLife);
 
     if (_life != previousLife) {
+      _updateStateLife();
       requestUpdate();
       onReceiveDamage(damage);
 
@@ -35,18 +42,25 @@ mixin Attackable on PositionedGameComponent {
     }
   }
 
-  void heal(double amount) {
+  void heal(int amount) {
     if (isDead) return;
 
     final previousLife = _life;
     _life = (_life + amount).clamp(0, _maxLife);
 
     if (_life != previousLife) {
+      _updateStateLife();
       requestUpdate();
     }
   }
 
-  void onReceiveDamage(double damage) {}
+  void _updateStateLife() {
+    if (this is UseState) {
+      (this as UseState).state.life = _life;
+    }
+  }
+
+  void onReceiveDamage(int damage) {}
 
   void onDie() {}
 }
